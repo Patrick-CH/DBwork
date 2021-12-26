2c9f85a1cf6837cdf41ed199661eee2ae1e0a5f8
Patrick-CH <1648109733@qq.com>
2021-12-27 01:39:31 +0800

    add model: BuyIn ClearOut

--- models.py
+++ models.py
@@ -5,7 +5,7 @@ from app import db
 
 class Stuff(db.Model):
     __tablename__ = "stuff"
-    SNo = db.Column(db.Integer, primary_key=True)
+    SNo = db.Column(db.Integer(), primary_key=True)
     Name = db.Column(db.String(20))
     Department = db.Column(db.String(20))
     Phone = db.Column(db.String(20))
@@ -44,14 +44,14 @@ class Equipment(db.Model):
 class LendAndBcak(db.Model):
     __tablename__ = "lendandback"
     LdBkNo = db.Column(db.Integer, primary_key=True)
-    ENo = db.Column(db.Integer)
-    LdDate = db.Column(db.Date)
-    ExpBkDate = db.Column(db.Date)
-    LdSNo = db.Column(db.Integer)
-    LdConfirm = db.Column(db.Integer)
-    LdMng = db.Column(db.Integer)
-    BkDate = db.Column(db.Integer)
-    BkMng = db.Column(db.Integer)
+    ENo = db.Column(db.Integer())
+    LdDate = db.Column(db.Date())
+    ExpBkDate = db.Column(db.Date())
+    LdSNo = db.Column(db.Integer())
+    LdConfirm = db.Column(db.Integer())
+    LdMng = db.Column(db.Integer())
+    BkDate = db.Column(db.Integer())
+    BkMng = db.Column(db.Integer())
 
     def __init__(self, LdBkNo, ENo, LdDate, ExpBkDate, LdSNo, LdConfirm, LdMng, BkDate, BkMng):
         self.LdBkNo = LdBkNo
@@ -68,8 +68,63 @@ class LendAndBcak(db.Model):
         return '<LendAndBk %r>' % self.LdBkNo
 
 
+class BuyIn(db.Model):
+    __tablename__ = "buyin"
+    InNo = db.Column(db.Integer(), primary_key=True)
+    ENo = db.Column(db.Integer())
+    BDepart = db.Column(db.String())
+    SNoBuy = db.Column(db.Integer())
+    InMng = db.Column(db.Integer())
+    InDate = db.Column(db.Date())
+    Fund = db.Column(db.String(20))
+    BConfirm = db.Column(db.Integer())
+
+    def __init__(self, InNo, ENo, BDepart, SNoBuy, InMng, InDate, Fund, BConfirm):
+        self.InNo = InNo
+        self.ENo = ENo
+        self.BDepart = BDepart
+        self.SNoBuy = SNoBuy
+        self.InMng = InMng
+        self.InDate = InDate
+        self.Fund = Fund
+        self.BConfirm = BConfirm
+
+    def __repr__(self):
+        return "<BuyIn %r>" % self.InNo
+
+
+class ClearOut(db.Model):
+    __tablename__ = "clearout"
+    OutNo = db.Column(db.Integer, primary_key=True)
+    ENo = db.Column(db.Integer, primary_key=True)
+    EName = db.Column(db.String(20))
+    Norm = db.Column(db.String(20))
+    Type = db.Column(db.String(20))
+    Manufacture = db.Column(db.String(20))
+    ScrapDate = db.Column(db.Date())
+    ScrapReason = db.Column(db.String(30))
+    SConfirm = db.Column(db.Integer())
+    OutMng = db.Column(db.Integer())
+
+    def __init__(self, OutNo, ENo, EName, Norm, Type, Manufacture, ScrapDate, ScrapReason, SConfirm, OutMng):
+        self.OutNo = OutNo
+        self.ENo = ENo
+        self.EName = EName
+        self.Norm = Norm
+        self.Type = Type
+        self.Manufacture =Manufacture
+        self.ScrapDate = ScrapDate
+        self.ScrapReason = ScrapReason
+        self.SConfirm = SConfirm
+        self.OutMng = OutMng
+
+    def __repr__(self):
+        return "<Scrap %r>" % self.OutNo
+
+
 if __name__ == '__main__':
-    ls = Stuff.query.all()
+    ls = BuyIn.query.filter().all()
+    print(ls)
     # ls = LendAndBcak.query.filter().all()
     # newLd = LendAndBcak(None, 2, datetime.date.today(), datetime.date.today(), 5, 6, 7, None, None)
     # for i in ls:
@@ -77,7 +132,7 @@ if __name__ == '__main__':
     #     print(type(i.LdDate))
     # print(ls)
     # print(datetime.date.today())
-    from sqlalchemy import update
-    u = update(Equipment).where(Equipment.ENo == 1)
-    u = u.values(Status=1)
-    print(u)
+    # from sqlalchemy import update
+    # u = update(Equipment).where(Equipment.ENo == 1)
+    # u = u.values(Status=1)
+    # print(u)
